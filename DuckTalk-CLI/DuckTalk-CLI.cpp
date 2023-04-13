137169e489d8bf4953d88eb76c98213a5f1e5bd5
Pfefan <stefanpfeiffer04@gmail.com>
2023-04-13 11:25:25 +0200

    get request using boost

--- DuckTalk-CLI/DuckTalk-CLI.cpp
+++ DuckTalk-CLI/DuckTalk-CLI.cpp
@@ -1,60 +1,79 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "lib/curl.h"
-#include "lib/json.hpp"
+#include <boost/asio.hpp>
 
-using json = nlohmann::json;
+using boost::asio::ip::tcp;
+
+void get_request() {
+    boost::asio::io_service io_service;
+
+    // Resolve DNS to get the IP address of the server
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query("example.com", "80");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    // Establish a connection to the server
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    // Send the HTTP request
+    boost::asio::streambuf request;
+    std::ostream request_stream(&request);
+    request_stream << "GET / HTTP/1.1\r\n";
+    request_stream << "Host: example.com\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connection: close\r\n\r\n";
+
+    boost::asio::write(socket, request);
+
+    // Read the response
+    boost::asio::streambuf response;
+    boost::asio::read_until(socket, response, "\r\n");
+
+    // Print the response
+    std::istream response_stream(&response);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    std::cout << "Response: " << http_version << " " << status_code << " " << status_message << std::endl;
+
+    // Print the response headers
+    boost::asio::read_until(socket, response, "\r\n\r\n");
+    std::string header;
+    while (std::getline(response_stream, header) && header != "\r")
+    {
+        std::cout << header << std::endl;
+    }
+    std::cout << std::endl;
+
+    // Print the response body
+    if (response.size() > 0)
+    {
+        std::cout << boost::asio::buffer_cast<const char*>(response.data()) << std::endl;
+    }
+}
 
 void handle_command(const std::string& cmd, const std::string& arg1)
 {
     if (cmd == "send") {
-        std::cout << "Sending: " << arg1 << std::endl;
+        // implement logic to send post
+        std::cout << "Sending post " << arg1 << std::endl;
+    }
 
-        std::string message = arg1;
-
-        json data = {
-            {"type", "message"},
-            {"auth", {
-                {"type", "token"},
-                {"token", "token"}
-            }},
-            {"data", {
-                {"receiver", {
-                    {"type", "user"},
-                    {"user_id", 92}
-                }},
-                {"content", message}
-            }}
-        };
-
-        std::string data_str = data.dump();
-
-        CURL* curl = curl_easy_init();
-
-        if (curl) {
-            curl_easy_setopt(curl, CURLOPT_URL, "http://ableytner.ddns.net:2006");
-
-            curl_easy_setopt(curl, CURLOPT_POST, 1L);
-
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data_str.c_str());
-
-            CURLcode res = curl_easy_perform(curl);
-
-            if (res != CURLE_OK) {
-                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-            }
-
-            curl_easy_cleanup(curl);
-        }
-
-
+    if (cmd == "get") {
+        get_request();
+        std::cout << "Sending get " << arg1 << std::endl;
     }
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc < 3)
+    get_request();
+    if (argc < 2)
     {
         std::cerr << "Usage: " << argv[0] << " [command]" << std::endl;
         return 1;
