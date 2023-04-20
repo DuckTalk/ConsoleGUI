72c02c47a5519b87b7e4df88fabc70f05ad6a94e
pfeiff190100 <pfeiff190100@sr.htlweiz.at>
2023-04-20 11:58:37 +0200

    implemented post request

--- DuckTalk-CLI/DuckTalk-CLI.cpp
+++ DuckTalk-CLI/DuckTalk-CLI.cpp
@@ -5,6 +5,7 @@
 #include "requests.hpp"
 
 
+
 std::unordered_map<std::string, std::string> read_config_file(const std::string& file_path)
 {
     std::ifstream file(file_path);
@@ -38,18 +39,43 @@ std::unordered_map<std::string, std::string> read_config_file(const std::string&
 }
 
 
-void handle_command(const std::string host, const std::string& cmd, const std::string& arg1)
+void handle_command(const std::string host, const std::string& cmd, const std::string& arg1, const std::string& arg2 = "", const std::string& arg3 = "")
 {
-    if (cmd == "send") {
-        // implement logic to send post
-        std::cout << "Sending post " << arg1 << std::endl;
+    if (cmd == "login" && arg2 != "" && arg3 != "") {
+        std::cout << "Loging in as  " << arg1 << "With the email: " << arg2 << std::endl;
+        // Implement logic to login the user
     }
 
-    if (cmd == "get") {
+    if (cmd == "send_message") {
+        std::cout << "Sending message " << arg1 << std::endl;
+        // implement logic to send post
+
+        // Define the API endpoint
+        std::string endpoint = "/posts";
+
+        // Define the request payload
+        std::string payload = R"(
+            {
+                "title": "foo",
+                "body": "bar",
+                "userId": 1
+            }
+        )";
+
+        // Make the HTTP POST request
+        HttpRequests request("jsonplaceholder.typicode.com");
+        std::string response = request.post_request(endpoint, payload);
+
+        // Print the response
+        std::cout << response << std::endl;
+
+    }
+
+    if (cmd == "get_messages") {
+        std::cout << "Getting messages " << arg1 << std::endl;
         HttpRequests request(host);
         std::string response = request.get_request();
         std::cout << response << std::endl;
-        std::cout << "Sending get " << arg1 << std::endl;
     }
 }
 
@@ -68,8 +94,10 @@ int main(int argc, char* argv[])
     // Handle the command
     std::string cmd = argv[1];
     std::string arg1 = argv[2];
+    std::string arg2 = argc > 3 ? argv[3] : "";
+    std::string arg3 = argc > 4 ? argv[4] : "";
 
-    handle_command(api_host, cmd, arg1);
+    handle_command(api_host, cmd, arg1, arg2, arg3);
 
     return 0;
 }
--- DuckTalk-CLI/requests.hpp
+++ DuckTalk-CLI/requests.hpp
@@ -1,13 +1,14 @@
 #pragma once
-#ifndef GET_REQUEST_HPP
-#define GET_REQUEST_HPP
+#ifndef HTTP_REQUESTS_HPP
+#define HTTP_REQUESTS_HPP
 
 #include <string>
 
 class HttpRequests {
 public:
-    HttpRequests(std::string host);
+    HttpRequests(std::string ip);
     std::string get_request();
+    std::string post_request(const std::string& path, const std::string& payload);
 protected:
     std::string host;
 };
